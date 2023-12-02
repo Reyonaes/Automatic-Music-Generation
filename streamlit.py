ddeb75e550b5a9938eebc01d1fe7e55319d3eba7
Reyonaes <140374133+Reyonaes@users.noreply.github.com>
2023-12-03 00:43:18 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,8 +16,8 @@ st.markdown("Classical music in Bach style")
 index=int(st.number_input("Enter a random number from 0 to 185"))
 df=pd.read_csv("Bach_dataframe.csv")
 x=df.iloc[:,:-1]
-x=np.array(x)
-x= np.reshape(x,(len(x),timesteps,1))
+x_new=np.array(x)
+x_new = np.reshape(x_new,(len(x_new),timesteps,1))
 music_pattern=x[index]
 model2=keras.models.load_model("Classical_Bach.h5")
 if st.button("Generate") :
