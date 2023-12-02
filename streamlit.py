ee2cd7b6791997bf9eeed4d4c3775508ac16ff96
Reyonaes <140374133+Reyonaes@users.noreply.github.com>
2023-12-02 22:13:19 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,7 +10,7 @@ from tensorflow import keras
 st.title("Automatic Music Generation")
 st.markdown("LSTM model which creates classical music by inputing a random number")
 n=st.number_input("Enter a random number from 0 to 37")
-model2=keras.load_model("Classical_Bach")
+model2=joblib.load("Classical_Bach2.sav")
 if st.button("Generate") :
 	music_pattern = x_test[n]
 	out_pred=[]
