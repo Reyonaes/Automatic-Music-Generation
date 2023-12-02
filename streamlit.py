9ad9d390e6a62aee57ad7d2f04b8c60ebdf7a8f7
Reyonaes <140374133+Reyonaes@users.noreply.github.com>
2023-12-03 00:19:52 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import music21
 from music21 import *
-import glob
+import pandas as pd
 import numpy as np
 import random
 import joblib
@@ -9,16 +9,12 @@ import tensorflow
 from tensorflow import keras
 st.title("Automatic Music Generation")
 st.markdown("Classical music in Bach style")
-unique_notes=['D3', 'D5', 'E5', 'F#5', 'A4', 'D4', 'A5', 'F#3', 'G5', 'E3', 'B4', 'C#5', 'A3', 'C#4', 'G#5', 'G#3', 'C#3', 'F#4', 'B2', 'G#4', 'E4', 'A2', 'G4', 'E-5', 'E-3', 'G2', 'B-4', 'F#2', 'F4', 'F5', 'B5', 'E2', 'C5', 'D2', 'C4', 'B3', 'C3', 'E-4', 'G3', 'B-3', 'F3', 'B-5', 'B-2', 'G#2', '2.5.8.9', '2.5.8.11', '11.1', '8.9', '4.6', '6.8', '9.11', '1.2', '4.5', '2.4', '1.4.7.10', '4.9', '9', '2.6.9', '6.7', '2.6', '9.1', '4.6.7', '11.1.2', '7.9', '6.9', '2.4.6', '4.7', '1.2.4', '11.2', '11.0', '7.11', '3.4', '9.0', '1.4']
-musical_note=st.text_input("Enter a musical note (Letters A to G)").upper()
-pattern=[]
-for i, ele in enumerate(unique_notes) :
-	 if musical_note in ele :
-		 for j in range(6) :
-			 pattern.append(i+j)
+index=int(st.number_input("Enter a random number from 0 to 185"))
+df=pd.read_csv("Bach_dataframe.csv")
+x=df.iloc[:,:-1]
+music_pattern=x[index]
 model2=keras.models.load_model("Classical_Bach.h5")
 if st.button("Generate") :
-	music_pattern = np.array(pattern)
 	out_pred=[]
 	for i in range(200):
 		music_pattern = music_pattern.reshape(1,len(music_pattern),1)
